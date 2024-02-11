8e71c14974d537cc6a557c59d0d45de95decb839
RoaaK95 <ruaakhuldoon.26@gmail.com>
2024-02-11 03:02:37 +0300

    Die state script
    Inherit from STATE class

---
+++ Assets/Scripts/States/Die.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Die : State
+{
+    public Die(GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player) : base(_npc, _agent, _anim, _player)
+    {
+        _name = STATE.DIE;
+    }
+
+    public override void Enter()
+    {
+        _anim.SetTrigger("isDead");
+        base.Enter();
+    }
+
+    public override void Update()
+    {
+        
+    }
+    public override void Exit() 
+    { 
+        base.Exit();
+    }
+}
